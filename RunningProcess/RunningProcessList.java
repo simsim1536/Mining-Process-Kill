1b1f1328dac58a8bc6bae48cf597812db0067a89
서대원 <hero1536@naver.com>
2018-01-13 22:54:18 +0900

    클래스 수정
    클래스명 수정

--- RunningProcess/RunningProcessList.java
+++ RunningProcess/RunningProcessList.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
  
-public class WindowsUtils {
+public class RunningProcessList {
     public static List listRunningProcesses(){
         List processes = new ArrayList();
         try{
@@ -26,30 +26,30 @@ public class WindowsUtils {
         return processes;
     }
  
-    public static void main(String[] args){
-        List processes = listRunningProcesses();
-        
-        //중복된 이름의 프로세스 하나만 표시
-        HashSet<String> distinctData = new HashSet<String>(processes);
-        processes = new ArrayList<String>(distinctData);
-        String result = "";
-        // display the result
-        Iterator it = processes.iterator();
-        int i = 0;
-        while (it.hasNext()){
-            result += it.next() + ",";
-            i++;
-            if (i == 10){
-                result += "\n";
-                i = 0;
-            }
-        }
- 
-        msgBox("Running processes : " + result);
+    public static void DisplayRunningProcess() {
+    	  List processes = listRunningProcesses();
+          
+          //중복된 이름의 프로세스 하나만 표시
+          HashSet<String> distinctData = new HashSet<String>(processes);
+          processes = new ArrayList<String>(distinctData);
+          String result = "";
+          // display the result
+          Iterator it = processes.iterator();
+          int i = 0;
+          while (it.hasNext()){
+              result += it.next() + "\n";
+              i++;
+              if (i == 10){
+                  result += "\n";
+                  i = 0;
+              }
+             
+          }
+          System.out.printf(result);
     }
- 
-    public static void msgBox(String msg){
-        javax.swing.JOptionPane.showConfirmDialog((java.awt.Component)null,
-                msg, "WindowsUtils", javax.swing.JOptionPane.DEFAULT_OPTION);
+    
+
+    public static void main(String[] args){
+    	DisplayRunningProcess();
     }
 }
