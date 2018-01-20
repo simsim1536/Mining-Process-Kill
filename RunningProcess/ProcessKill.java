f37da74614f7ef987f10c23a1a000a1670a5fbc9
서대원 <hero1536@naver.com>
2018-01-20 21:17:07 +0900

    메소드 추가
    채굴 프로세스 종료 메소드 추가

--- RunningProcess/ProcessKill.java
+++ RunningProcess/ProcessKill.java
@@ -4,14 +4,30 @@ import java.io.IOException;
 
 public  class ProcessKill{
 
+	static MainFrame Main = new MainFrame();
+
 	//프로세스명에 해당하는 프로세스 종료
 	public static void ProcsKill(){
+		//String P=Main.name;
+		String K = "taskkill /F /IM notepad.exe";
+		//String PK = P + K;
 		try{
-			Runtime.getRuntime().exec("taskkill /F /IM 프로세스명");
+			Runtime.getRuntime().exec(K);
 		}
 		catch(IOException e)
 		{
 			e.printStackTrace();
 		}	
 		}
+
+	//채굴 프로세스 종료
+	public static void MiningProcsKill(){
+	try{
+		Runtime.getRuntime().exec("taskkill /F /IM 프로세스명");
+	}
+	catch(IOException e)
+	{
+		e.printStackTrace();
+	}	
+	}
 }
